04aea48b9e67cdfb9dbd5095880d210ba6d8b20d
Kael <piskael03@gmail.com>
2019-01-08 10:47:57 -0500

    test

--- packages/server/index.js
+++ packages/server/index.js
@@ -33,8 +33,9 @@ mongoose
 	)
 	.then(() => console.log('MongoDB database connected..'))
 	.catch(err =>
-		console.log(`Errors occurred when
-       tried to connect the database. ${err}`)
+		console.log(
+			`Errors occurred when tried to connect the database dsadasdasdsadsadsadas. ${err}`
+		)
 	);
 
 const PORT = process.env.PORT || 5000;
